c8e47a47439ed940dabc3e95850cf3da5658bae5
Kon Akech <76453981+Konson22@users.noreply.github.com>
2023-11-22 01:12:14 +0300

    Update productController.js
    replaced this file

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,14 +2,10 @@ const sqlite = require('sqlite3').verbose();
 
 const db = new sqlite.Database("./database.db", sqlite.OPEN_READWRITE, err => err && console.log(err));
 
-
-
 // CREATE RESUME
 const addProduct = async (req, res) => {
   const { name, category, qty, price, description } = req.body
-
   const product_image = req.imageUrl;
-
   sql = 'INSERT INTO products(name, category, quantity, price, description, product_image) VALUES(?, ?,?,?,?,?)';
 
   db.run(sql, [name, category, qty, price, description, req.imageUrl], async function(err){
@@ -62,7 +58,7 @@ const deleteProducts = (req, res) => {
 const addReserveProduct = async function(req, res){
   const code = generateCode()
   const { cartData, collectionTime, collectionMethod } = req.body
-// console.log(cartData, collectionTime, collectionMethod)
+  const price = cartData.map(i => i.price).reduce((a, t) => +a + +t)
   try {
     cartData.forEach(el => {
       sql = 'INSERT INTO reservation(product_name, qty, price, product_image, code, served, collectionTime, collectionMethod) VALUES(?,?,?,?,?,?,?,?)';
@@ -72,20 +68,30 @@ const addReserveProduct = async function(req, res){
     });
     res.json({
       done:true,
-      cart:{code, items:cartData.length, price, collectionTime, collectionMethod}
+      cart:{code, items:cartData.length, price, collectionTime:collectionTime, collectionMethod}
     });
   } catch (error) {
     res.status(500).json({done:false});
   }
 }
 
+const deleteReservation = (req, res) => {
+  try{
+    db.all(`DELETE FROM reservation WHERE code = ${req.body.code}`, (err) => {
+      if(err) throw err;
+      res.json(req.body.code)
+    })
+  }catch(error){
+    console.log(error)
+  }
+}
 function generateCode(){
   return Math.floor(1000 + Math.random() * 9000);
 }
 
 const getReserveProducts = (req, res) => {
   try{
-    db.all('SELECT * FROM reservation', [], (err, rows) => {
+    db.all('SELECT * FROM reservation ORDER BY collectionTime DESC', [], (err, rows) => {
       if(err) throw err;
       res.json(rows)
     })
@@ -108,7 +114,7 @@ const getSingleReserve = (req, res) => {
 }
 
 
-// db.run("delete from reservation")
+// db.run("update reservation set served = 0")
 // db.run("ALTER TABLE reservation RENAME COLUMN isServed TO served")
 
 const checkoutReserve = (req, res) => {
@@ -131,9 +137,9 @@ module.exports = {
   editProduct,
   getAllProducts, 
   getReserveProducts, 
+  deleteReservation,
   addReserveProduct, 
   getSingleReserve, 
   checkoutReserve,
   deleteProducts
 }
-
